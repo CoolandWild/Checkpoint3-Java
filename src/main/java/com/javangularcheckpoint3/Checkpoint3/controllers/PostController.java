e48f85bf2f4486f45c2c2a081a75f7fd756ff435
CoolandWild <eric.boileaux@sncf.fr>
2021-01-11 11:08:25 +0100

    modifié 1 ligne controller

--- src/main/java/com/javangularcheckpoint3/Checkpoint3/controllers/PostController.java
+++ src/main/java/com/javangularcheckpoint3/Checkpoint3/controllers/PostController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class PostController {
 	}
 	
 	@GetMapping("/{id}")
-	public Posts read(@RequestParam(value="id") Long id) {
+	public Posts read(@PathVariable Long id) {
 		return postService.getPostsById(id);
 	}
 }
--- src/main/java/com/javangularcheckpoint3/Checkpoint3/controllers/UserController.java
+++ src/main/java/com/javangularcheckpoint3/Checkpoint3/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/{id}")
-	public User read(@RequestParam(value="id") Long id) {
+	public User read(@PathVariable Long id) {
 		return userService.getUserById(id);
 	}
 
